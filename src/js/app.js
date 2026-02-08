8678f76d690d02417e024a3498eb998b109ac82e
whoisleshalight <whoisleshalight@gmail.com>
2026-02-08 23:50:57 +0200

    u

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,17 @@
 // Підключення функціоналу "Чортоги Фрілансера"
-import { addTouchAttr, addLoadedAttr, isMobile, FLS } from "@js/common/functions.js"
+import {
+	addTouchAttr,
+	addLoadedAttr,
+	isMobile,
+	FLS
+} from "@js/common/functions.js"
 addLoadedAttr();
+
+if (isMobile.any()) {
+	const btnSublist = document.querySelector('.btn-sublist');
+	if (btnSublist) {
+		btnSublist.addEventListener("click", function (e) {
+			btnSublist.nextElementSibling.classList.toggle('_active');
+		});
+	}
+}
